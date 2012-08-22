23f5d995fb7c9d48148200096f56e1cbf9e66b2e
Dr Blinken <drblinken@gmail.com>
2012-08-22 11:38:18 +0200

    wuerfel game before exercise

--- eclipse/BlueJ/src/agame/Game.java
+++ eclipse/BlueJ/src/agame/Game.java
@@ -1,24 +1,25 @@
 package agame;
 
+import agame.wuerfel.Wuerfel;
+
 public class Game {
 
 	public static void main(String[] argumente) {
-		System.out.println("Hallo Welt!");
-		System.out.println("Hallo Welt! hat " + "Hallo Welt!".length()
-				+ " Buchstaben");
-		Wuerfel w1;
-		Wuerfel w2;
-		
-		
-		int wuerfel1_augen = werfeWuerfel();
-		int wuerfel2_augen = werfeWuerfel();
 
-		System.out.println("Die WŸrfel: " + wuerfel1_augen + " | "
-				+ wuerfel2_augen);
+		while (true) {
+			System.out.println("Hallo Welt!");
+
+			Wuerfel wuerfel1 = new Wuerfel();
+			Wuerfel wuerfel2 = new Wuerfel();
+
+			wuerfel1.werfe();
+			wuerfel2.werfe();
+
+			System.out.println("Die WŸrfel: " + wuerfel1.augen() + " | "
+					+ wuerfel2.augen());
+		}
 	}
 
-	public static int werfeWuerfel() {
-		return (int) (Math.random() * 6) + 1;
-
-	}
 }
+
+// int inChar = System.in.read();
